b9124ed1779ccf42c234a2535d1916b6b624d943
JulianoHiroi <julianohiroi@gmail.com>
2024-03-15 17:41:58 -0300

    Make animation of Character

---
+++ RogueLikeGame/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Animator playerAnimator;
+    float input_x = 0;
+    float input_y = 0;
+    public float speed = 2.5f;
+    bool isWalking = false;
+
+    // Start is called before the first frame update
+    
+    void Start()
+    {
+        isWalking = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        input_x = Input.GetAxis("Horizontal");
+        input_y = Input.GetAxis("Vertical");
+        isWalking = (input_x != 0 || input_y != 0);
+
+
+        if(isWalking){
+            var move = new Vector3(input_x, input_y, 0).normalized;
+            transform.position += move * speed * Time.deltaTime;
+            playerAnimator.SetFloat("input_x", input_x);
+            playerAnimator.SetFloat("input_y", input_y);
+
+        }
+        playerAnimator.SetBool("isWalking", isWalking);
+
+        if(Input.GetKeyDown(KeyCode.Space)){
+            playerAnimator.SetTrigger("atack");
+        }
+    }
+}
